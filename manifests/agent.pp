1571cb18195516a57f5998d97b56d080d63760d1
John Florian <jflorian@doubledog.org>
2017-02-03 18:52:04 -0500

    New - Zabbix module
    Thus far this module provides support for the Zabbix agent only.

---
+++ manifests/agent.pp
@@ -0,0 +1,85 @@
+# modules/zabbix/manifests/agent.pp
+#
+# == Class: zabbix::agent
+#
+# Manages the Zabbix agent on a host.
+#
+# === Parameters
+#
+# ==== Required
+#
+# ==== Optional
+#
+# [*enable*]
+#   Instance is to be started at boot.  Either true (default) or false.
+#
+# [*ensure*]
+#   Instance is to be 'running' (default) or 'stopped'.  Alternatively,
+#   a Boolean value may also be used with true equivalent to 'running' and
+#   false equivalent to 'stopped'.
+#
+# === Authors
+#
+#   John Florian <jflorian@doubledog.org>
+#
+# === Copyright
+#
+# Copyright 2017 John Florian
+
+
+class zabbix::agent (
+        String $conf_file,
+        Boolean $enable,
+        Variant[Boolean, Enum['running', 'stopped']] $ensure,
+        Boolean $manage_firewall,
+        Array[String] $packages,
+        String $server_ip,
+        Integer[1,65535] $listen_port,
+        Array[String] $services,
+    ) {
+
+    package { $::zabbix::agent::packages:
+        ensure => installed,
+        notify => Service[$::zabbix::agent::services],
+    }
+
+    file {
+        default:
+            owner     => 'root',
+            group     => 'root',
+            mode      => '0644',
+            seluser   => 'system_u',
+            selrole   => 'object_r',
+            seltype   => 'etc_t',
+            before    => Service[$::zabbix::agent::services],
+            notify    => Service[$::zabbix::agent::services],
+            subscribe => Package[$::zabbix::agent::packages],
+            ;
+        '/etc/zabbix/zabbix_agentd.d':
+            ensure => directory,
+            ;
+        $conf_file:
+            content => template('zabbix/zabbix_agentd.conf.erb'),
+            ;
+    }
+
+    if $manage_firewall {
+        firewall {
+            '300 accept Zabbix server':
+                source => $server_ip,
+                dport  => $listen_port,
+                proto  => 'tcp',
+                state  => 'NEW',
+                action => 'accept',
+                ;
+        }
+    }
+
+    service { $::zabbix::agent::services:
+        ensure     => $ensure,
+        enable     => $enable,
+        hasrestart => true,
+        hasstatus  => true,
+    }
+
+}
